537a30ea13c0a1cbe96aff731443a1025913ce9d
Javlonbek <example@gmail.com>
2022-08-27 11:52:50 +0500

    Random Password

--- main.cpp
+++ main.cpp
@@ -23,33 +23,17 @@ bool isLoggedIn(){
 };
 
 int main(){
-    int choice;
-    Menu:
-    std::cout<<"1.Register: \n2.Login: \nYour choice: ";
-    std::cin>>choice;
-    if(choice==1){
-       std::string username, password;
-       std::cout<<"Enter Username: "; std::cin>>username;
-       std::cout<<"Enter Password: "; std::cin>>password;
+    srand(time(0));
+    std::string pass;
 
-       std::ofstream file;
-       file.open("datas\\"+username+".txt");
-       file<<username<<std::endl<<password;
-       file.close();
-       goto Menu;
-
-
-    }else if(choice==2){
-        bool status = isLoggedIn();
-            if(!status){
-                std::cout<<"No registered!!!"<<std::endl;
-
-                return 0;
-            } else std::cout<<"Welcome!!!";
-    }else
-    {
-        std::cout<<"Enter only 1 or 2 "<<std::endl;
+    std::string data = "abcdefghyjklmnopqrstuvwxyz"
+                       "ABCDEFGHYJKLMONPQRSTUVWXYZ"
+                       "0123456789";
+    for(int i=1; i<=12; i++){
+        pass=pass+data[rand()%93];
     }
+    std::cout<<"Generated Password: "<<pass<<std::endl;
+
 
 
     return 0;
